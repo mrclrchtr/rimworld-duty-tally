2f2bf248b9b640c89d00f59b554abb3017e14420
Marcel Richter <mail@mrclrchtr.de>
2025-04-22 22:21:06 +0200

    feat(settings): improve settings handling and add symbol package support

--- Source/DutyTallyMod.cs
+++ Source/DutyTallyMod.cs
@@ -6,11 +6,25 @@ namespace mrclrchtr.DutyTally.Source
     // ReSharper disable once ClassNeverInstantiated.Global
     public class DutyTallyMod : Mod
     {
-        public static DutyTallySettings Settings;
+        private static DutyTallySettings? _settings;
+
+        public static DutyTallySettings Settings
+        {
+            get
+            {
+                if (_settings == null)
+                {
+                    throw new InvalidOperationException("Settings accessed before initialization.");
+                }
+
+                return _settings;
+            }
+        }
+
 
         public DutyTallyMod(ModContentPack content) : base(content)
         {
-            Settings = GetSettings<DutyTallySettings>();
+            _settings = GetSettings<DutyTallySettings>();
         }
 
         public override string SettingsCategory()
@@ -20,23 +34,29 @@ namespace mrclrchtr.DutyTally.Source
 
         public override void DoSettingsWindowContents(Rect inRect)
         {
+            if (_settings == null)
+            {
+                Log.Error("DutyTally settings are null in DoSettingsWindowContents. This should not happen.");
+                return; // Don't try to draw the UI if settings are missing
+            }
+
             var listingStandard = new Listing_Standard();
             listingStandard.Begin(inRect);
             listingStandard.CheckboxLabeled("DutyTally_IgnoreInvisibleWorkTypes".Translate(),
-                ref Settings.IgnoreInvisibleWorkTypes, "DutyTally_IgnoreInvisibleWorkTypesTip".Translate());
+                ref _settings.IgnoreInvisibleWorkTypes, "DutyTally_IgnoreInvisibleWorkTypesTip".Translate());
             listingStandard.Gap();
             listingStandard.CheckboxLabeled("DutyTally_UseWeightedPriorities".Translate(),
-                ref Settings.UseWeightedPriorities, "DutyTally_UseWeightedPrioritiesTip".Translate());
+                ref _settings.UseWeightedPriorities, "DutyTally_UseWeightedPrioritiesTip".Translate());
             listingStandard.Gap();
 
             listingStandard.Gap();
-            string maxPriorityBuffer = Settings.MaxPriorityForWeighting.ToString();
+            string maxPriorityBuffer = _settings.MaxPriorityForWeighting.ToString();
             Rect numericFieldRect = listingStandard.GetRect(Text.LineHeight);
             Rect labelRect = numericFieldRect.LeftPart(0.8f).Rounded();
             Rect fieldRect = numericFieldRect.RightPart(0.2f).Rounded();
             Widgets.Label(labelRect, "DutyTally_MaxPriorityForWeighting".Translate());
             TooltipHandler.TipRegion(labelRect, "DutyTally_MaxPriorityForWeightingTip".Translate());
-            Widgets.TextFieldNumeric<int>(fieldRect, ref Settings.MaxPriorityForWeighting, ref maxPriorityBuffer, 1, 99);
+            Widgets.TextFieldNumeric(fieldRect, ref _settings.MaxPriorityForWeighting, ref maxPriorityBuffer, 1, 99);
 
             listingStandard.End();
             base.DoSettingsWindowContents(inRect);
